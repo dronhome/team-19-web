db82f95574ac744d87e58aab8d3b3b222e6fdb5a
Andrii Kostiushko <dron.home13@gmail.com>
2025-10-18 19:33:44 +0200

    telify try 1

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+<<<<<<< HEAD
   output: "export",
   images: { unoptimized: true },
   // only set basePath when building for GitHub Pages
   basePath: process.env.NEXT_PUBLIC_TARGET === "gh-pages" ? "/team-19-web" : "",
+=======
+    output: "export",
+    images: { unoptimized: true },
+    // only set basePath when building for GitHub Pages
+    basePath: process.env.NEXT_PUBLIC_TARGET === "gh-pages" ? "/team-19-web" : "",
+>>>>>>> 6eab86c (telify try 1)
 };
 
 export default nextConfig;
