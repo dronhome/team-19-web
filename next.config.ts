a295dbcfa5fd73b5836d2ed45f3b0142783839ff
Andrii Kostiushko <dron.home13@gmail.com>
2025-10-18 19:27:09 +0200

    Refactor next.config.ts for cleaner configuration

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,14 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === "production";
-<<<<<<< HEAD
-const repo = "team-19-web"; // your repo name
+const repo = "team-19-web";
 
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
-  // Use ONLY basePath for GitHub Project Pages
   basePath: isProd ? `/${repo}` : "",
-  // REMOVE assetPrefix to avoid double prefixing of CSS/JS on Pages
-  // assetPrefix: isProd ? `/${repo}/` : "",
-=======
-const repo = "team-19-web";
-
-const nextConfig: NextConfig = {
-    output: "export",
-    images: { unoptimized: true },
-    basePath: isProd ? `/${repo}` : "",
->>>>>>> feb714e (Fix: logo)
 };
 
 export default nextConfig;
+
