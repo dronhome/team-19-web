55e11e7a1fa82e526251636c036331e69feb537b
Andrii Kostiushko <dron.home13@gmail.com>
2025-10-18 19:02:04 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+// If publishing at https://dronhome.github.io/team-19-web/
+const repo = "team-19-web";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",            // enables `next export`
+  images: { unoptimized: true } // needed for next/image with export
 };
 
 export default nextConfig;
